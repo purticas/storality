c4cecc55df6d93ea5bff5cfe1d5a407b3b87ba0f
Sebastian Opperman <oppermansebastian@gmail.com>
2024-09-29 12:13:20 -0700

    setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,10 +12,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Storality CMS"))
 }
 
+func pageView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ViewPage"))
+}
+
+func pageCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create Page"))
+}
+
 func main() {
 	fmt.Printf("Runing server on %s\n", port)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
+	mux.HandleFunc("/{$}", index)
+	mux.HandleFunc("/page/view", pageView)
+	mux.HandleFunc("/page/create", pageCreate)
 
 	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
